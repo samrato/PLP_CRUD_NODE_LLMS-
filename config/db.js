e269204d170721916e0ff3ff5139004e5c5ca2d9
Willington Juma <willingtonejumamconyango@gmail.com>
2025-09-10 21:55:47 +0300

    database connection done using connection string

--- config/db.js
+++ config/db.js
@@ -4,15 +4,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
 });
 
 pool.connect()
   .then(() => console.log(" Database connected successfully"))
-  .catch(err => console.error("Database connection error", err));
+  .catch(err => console.error("Database connection error:", err));
 
 module.exports = pool;
